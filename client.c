8e8a1d852d331ccdbee7d1af7207abf35275070b
devstar1224 <dev_sang@naver.com>
2019-10-02 11:20:19 +0900

    ADD 5-1, 5-2 English Ver

--- client.c
+++ client.c
@@ -3,37 +3,41 @@
 #include <sys/socket.h>
 #include <string.h>
 
-#define PORT 9000
+#define PORT 10000
 #define IPADDR "127.0.0.1"
+#define BUFSIZE 100
 
 int main(){
-	int c_socket;
+	int c_socket; 
 	struct sockaddr_in c_addr;
-	int len;
 	int n;
-	char rcvBuffer[BUFSIZ];
-	c_socket = socket(PF_INET, SOCK_STREAM, 0);
+	char rcvBuffer[BUFSIZE];
+	char sendBuffer[BUFSIZE] = "Hi, I'm client.";
+	c_socket = socket(PF_INET, SOCK_STREAM, 0); 
 	memset(&c_addr, 0, sizeof(c_addr));
-	c_addr.sin_addr.s_addr = inet_addr(IPADDR);
+	c_addr.sin_addr.s_addr = inet_addr(IPADDR); 
 	c_addr.sin_family = AF_INET;
 	c_addr.sin_port = htons(PORT);
-	if(connect(c_socket, (struct sockaddr*) &c_addr, sizeof(c_addr))==-1){
-		printf("fail connect \n");
+
+	if(connect(c_socket, (struct sockaddr *) &c_addr, sizeof(c_addr)) == -1){
+		printf("Cannot Connect\n");
 		close(c_socket);
-		return -1;
+		return -1; 
 	}
 	while(1){
-		char str_s[100];
-		fgets(str_s,255, stdin);
-		write(c_socket, str_s, strlen(str_s));
-		if((n = read(c_socket, rcvBuffer, sizeof(rcvBuffer))) < 0 ){
-			printf("error");	
+		fgets(sendBuffer, sizeof(sendBuffer), stdin);
+		write(c_socket, sendBuffer, strlen(sendBuffer));
+		if(strncasecmp(sendBuffer, "quit", 4) == 0 || strncasecmp(sendBuffer, "kill server", 11) == 0)
+			break;
+		n = read(c_socket, rcvBuffer, sizeof(rcvBuffer)); 
+		if (n < 0){
+			printf("Read Failed\n");
 			return -1;
 		}
 		rcvBuffer[n] = '\0';
-		printf("rcv len : %d\n", strlen(rcvBuffer));
-		printf("Data : %s \n", rcvBuffer);
+		printf("received data: %s\n", rcvBuffer);
+		printf("rcvBuffer length: %d\n", n);
 	}
-	printf("1");
-	// close(c_socket);
-} 
+	close(c_socket);
+	return 0;	
+}
--- server.c
+++ server.c
@@ -3,57 +3,61 @@
 #include <sys/socket.h>
 #include <string.h>
 
-#define PORT 9000
+#define PORT 10000
 
-char buffer[100] = "hello,world";
+char buffer[100] = "Hi, I'm server.\n";
+char rcvBuffer[100];
+int main(){
+	int c_socket, s_socket;
+	struct sockaddr_in s_addr, c_addr;
+	int len;
+	int n;
 
-int main()
-{
-int c_socket, s_socket;
-struct sockaddr_in s_addr, c_addr;
-int len;
-int n;
-char rcvBuffer[BUFSIZ];
+	s_socket = socket(PF_INET, SOCK_STREAM, 0);
+	
+	memset(&s_addr, 0, sizeof(s_addr)); 
+	s_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	s_addr.sin_family = AF_INET;
+	s_addr.sin_port = htons(PORT);
 
 
-s_socket = socket(PF_INET, SOCK_STREAM, 0);
-memset(&s_addr, 0, sizeof(s_addr));
-s_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-s_addr.sin_family = AF_INET;
-s_addr.sin_port = htons(PORT);
-
-if(bind(s_socket, (struct sockaddr *) &s_addr, sizeof(s_addr)) == -1){
-	printf("can't bind \n");
-	return -1;
-}
-if(listen(s_socket, 5) == -1)
-{
-	printf("listen fail\n");
-	return -1;
-}
-
-while(1){
-	len = sizeof(c_addr);
-	c_socket = accept(s_socket, (struct sockaddr *) &c_addr, &len);
-	printf("Client is connected\n");
-	while(1){
-		read(c_socket, rcvBuffer, sizeof(rcvBuffer));
-		printf("%s \n", rcvBuffer);
-		//printf("pass");
-		write(c_socket, rcvBuffer, strlen(rcvBuffer));
-		//printf("middle");
-		if(strcmp(rcvBuffer,"quit")==0)
-		{		
-			printf("quit");
-			break;
-		}	
-		if(strcmp(rcvBuffer,"kill server")==0){
-			return 0;
-		//	printf("kill");
-		}
-		//printf("last");
+	if(bind(s_socket,(struct sockaddr *) &s_addr, sizeof(s_addr)) == -1){ 
+		printf("Cannot Bind\n");
+		return -1;
 	}
-	close(c_socket);
-}
-close(s_socket);
+	
+	if(listen(s_socket, 5) == -1){
+		printf("listen Fail\n");
+		return -1;
+	}
+	while(1){
+		len = sizeof(c_addr);
+		printf("Wait connect client\n");
+		c_socket = accept(s_socket, (struct sockaddr *)&c_addr, &len); 
+		printf("/client is connected\n");
+		while(1){
+			n = read(c_socket, rcvBuffer, sizeof(rcvBuffer));
+			printf("rcvBuffer: %s\n", rcvBuffer);
+			
+			if(!strncasecmp(rcvBuffer, "hello", 5)){
+				char rcvBf[100] = "Hello, nice meet you";
+				write(c_socket, rcvBf, strlen(rcvBf));	
+			}else if(!strncasecmp(rcvBuffer, "what is your name?", 18)){
+				char rcvBf[100] = "My name is Linux!";
+				write(c_socket, rcvBf, strlen(rcvBf));
+			}else if(!strncasecmp(rcvBuffer, "How old are you?", 16)){
+				char rcvBf[100] = "I'm 59 years old";
+				write(c_socket, rcvBf, strlen(rcvBf));
+			}else if(!strncasecmp(rcvBuffer, "quit", 4)|| !strncasecmp(rcvBuffer, "kill server", 11)){
+				break;
+			}else
+			 write(c_socket, "Error", 5); 
+		}
+
+		close(c_socket);
+		if (strncasecmp(rcvBuffer, "kill server", 11) == 0)
+			break;
+	}
+	close(s_socket);
+	return 0;	
 }
